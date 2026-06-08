ab5c2a4679df2c39ee4f70ed87f9f5ca4720c674
Tarık Kavaz <tarikkavaz@gmail.com>
2026-06-08 10:12:57 +0300

    Add window, tab, search, theme, and other settings to options.lua for enhanced configuration

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,26 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
---vim.opt.showtabline = 0
+
+-- window settings
 vim.opt.winbar = "%=%m %f"
+
+-- tab settings
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+
+-- search settings
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- theme settings
+vim.opt.termguicolors = true
+vim.opt.background = "dark"
+vim.opt.signcolumn = "yes"
+
+-- other settings
+vim.opt.backspace = "indent,eol,start"
+vim.opt.clipboard = "unnamedplus"
